8d7779400a573a5715b06e6cad2b752a80532aec
Matt Way <matt__way@hotmail.com>
2017-02-11 22:57:56 +1000

    starting graphql tests

--- model/mutations/update_channel.js
+++ model/mutations/update_channel.js
@@ -1,25 +1,33 @@
+import { or } from 'twobyfour'
 import channelType from '/model/types/channel'
-import { isChannelAdmin } from '/model/permissions'
+import { isAdmin, isChannelAdmin } from '/model/permissions'
 
 export default {
   name: 'updateChannel',
   description: 'update an existing channel',
   type: channelType.read,
   args: {
+    slug: channelType.keys.slug,
     channel: {
       type: channelType.input,
-      description: 'The input channel object details'
+      description: 'The updated channel object details'
     }
   },
-  permissions: isChannelAdmin,
-  resolve (root, { channel }, { DB }) {
+  permissions: or([isAdmin, isChannelAdmin]),
+  resolve (root, { slug, channel }, { DB }) {
     return DB.updateResource({
       TableName: channelType.table,
-      Key: { slug: channel.slug },
+      Key: { slug },
       Expected: {
         slug: { Exists: true }
       },
       SET: channel
-    }).then(() => channel)
+    }).then(() => ({ slug, ...channel }))
+      .catch(e => {
+        if(e.code === 'ConditionalCheckFailedException'){
+          e.message = `Channel with slug (${slug}) does not exist`
+        }
+        throw e
+      })
   }
 }
---
+++ test/model/mutations/create_channel.spec.js
@@ -0,0 +1,15 @@
+
+describe('Test channel creation', () => {
+
+  it('should successfully create a channel', () => {
+
+  })
+
+  it('should not be able to create using existing slug', () => {
+
+  })
+
+  it('should fail creation if not using an admin token', () => {
+    
+  })
+})
