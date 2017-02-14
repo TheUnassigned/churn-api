d1f44b1585625473416d98400af919f5e858ec34
Matt Way <matt__way@hotmail.com>
2017-02-14 20:23:45 +1000

    channel create tests passing

--- test/model/mutations/create_channel.spec.js
+++ test/model/mutations/create_channel.spec.js
@@ -1,44 +1,81 @@
-import AWS from 'aws-sdk'
-import DB from '/dynamodb'
-import { config } from '/config/environment'
 import mock from '/dynamodb/mock'
-import channelType from '/model/types/channel'
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
+import schema from '/model'
+import AWS from 'aws-sdk'
+import DB from '/dynamodb'
+import { graphql } from 'graphql'
+import { config } from '/config/environment'
 
 chai.use(chaiAsPromised)
 chai.should()
 
-// init the database client
-DB.setDoc(new AWS.DynamoDB.DocumentClient({ region: config.AWS_REGION }))
+const expect = chai.expect
 
 // wait until the mocks have initialised before running any tests
 beforeEach(() => {
   return mock
 })
 
-describe('Test channel creation', () => {
+// init the database client
+DB.setDoc(new AWS.DynamoDB.DocumentClient({ region: config.AWS_REGION }))
+
+// construct the context object for each call to simulate an admin
+const context = {
+  DB,
+  viewer: { admin: true }
+}
+
+describe('Testing channel creation', () => {
+
+  // channel creation mutation
+  const mutation = `
+    mutation {
+      createChannel(slug: "testslug", channel: {
+        title: "test title"
+        external_links: {
+          url: "http://www.thingo.com"
+          facebook: "http://www.facebook.com/yoyoyo"
+        }
+        blurb: "This channel is a test channel"
+        logo_url: "http://logo.com"
+      }){
+        slug
+        title
+        external_links {
+          url
+          instagram
+        }
+      }
+    }
+  `
 
   it('should successfully create a channel', () => {
-    const item = {
-      slug: 'create_test',
-      title: 'create test title'
-    }
-    // create and read the created item
-    return DB.putResource({
-      TableName: channelType.table,
-      Item: item
-    }).then(() => DB.getResource({
-      TableName: channelType.table,
-      Key: { slug: item.slug }
-    })).then(result => result.should.deep.equal(item)).should.be.fulfilled
+    return graphql(schema, mutation, {}, context)
+      .then(result => {
+        const channel = result.data.createChannel
+        expect(channel).to.deep.equal({
+          slug: 'testslug',
+          title: 'test title',
+          external_links: {
+            url: 'http://www.thingo.com',
+            instagram: null
+          }
+        })
+      }).should.be.fulfilled
   })
 
   it('should not be able to create using existing slug', () => {
-    throw new Error('incomplete test')
+    return graphql(schema, mutation, {}, context)
+      .then(({ errors }) => {
+        if(errors){ return Promise.reject(errors) }
+      }).should.be.rejected
   })
 
   it('should fail creation if not using an admin token', () => {
-    throw new Error('incomplete test')
+    return graphql(schema, mutation, {}, { DB, viewer: {} })
+      .then(({ errors }) => {
+        if(errors){ return Promise.reject(errors) }
+      }).should.be.rejected
   })
 })
